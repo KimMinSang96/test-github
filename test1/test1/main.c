2e306a8a1b5eec12834362c64b772b0dcebbf1ac
KimMinSang <kimmsang96@gmail.com>
2022-06-01 04:34:35 +0900

    Title : test git commit

---
+++ test1/test1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test1
+//
+//  Created by 김민상 on 2022/06/01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("test11\n");
+    return 0;
+}
---
+++ test2/test2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test2
+//
+//  Created by 김민상 on 2022/06/01.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("test2");
+    return 0;
+}
